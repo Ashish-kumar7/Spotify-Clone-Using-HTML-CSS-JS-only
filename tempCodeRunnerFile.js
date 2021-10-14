4f7d19e9ff3d24caf516ec513d1319de3c5619d6
ashish007kumar <ashishkumar357ak@gmail.com>
2021-10-14 14:31:14 +0530

    files

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,20 @@
+const makeallPlay=()=>{
+//    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+//         element.classList.remove('fa-pause-circle');
+//         element.classList.add('fa-play-circle');
+//    })
+// }
+
+// Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+//     element.addEventListener('click', (e)=>{
+//         if(e.target.classList[2]==="fa-pause-circle"){
+//             e.target.classList.remove('fa-pause-circle');
+//             e.target.classList.add('fa-play-circle');
+//         }else{
+//             makeallPlay();
+//             e.target.classList.remove('fa-play-circle');
+//             e.target.classList.add('fa-pause-circle');
+//             // audioElement.src="./songs/9.mp3";
+//         }
+//     })
+// })
